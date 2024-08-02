93dec9d80144420e7b78bd956a45a2bc45cbc4d0
Shahabuddin1122 <shavoddin54@gmail.com>
2024-08-03 04:34:32 +0600

    update home page

---
+++ hand_gesture/lib/screens/after_connect_home_nav.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:hand_gesture/screens/graph.dart';
+import 'package:hand_gesture/screens/home.dart';
+import 'package:hand_gesture/screens/home_connected.dart';
+import 'package:hand_gesture/screens/learn.dart';
+import 'package:hand_gesture/screens/profile.dart';
+import 'package:hand_gesture/screens/tutorial.dart';
+import 'package:hand_gesture/utils/constants.dart';
+
+class AfterConnectHomeNav extends StatefulWidget {
+  const AfterConnectHomeNav({super.key});
+
+  @override
+  State<AfterConnectHomeNav> createState() => _LandingState();
+}
+
+class _LandingState extends State<AfterConnectHomeNav> {
+  int _selectedIndex = 2;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget bodyWidget;
+    switch (_selectedIndex) {
+      case 0:
+        bodyWidget = const Tutorial();
+        break;
+      case 1:
+        bodyWidget = const Graph();
+        break;
+      case 2:
+        bodyWidget = const HomeConnected();
+        break;
+      case 3:
+        bodyWidget = const Learn();
+        break;
+      case 4:
+        bodyWidget = const Profile();
+        break;
+      default:
+        bodyWidget = const Home();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: PRIMARY_COLOR,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications,
+              size: 30,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: bodyWidget,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[100],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: Colors.grey[600],
+        selectedFontSize: 16,
+        unselectedFontSize: 14,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.play_circle,
+              size: _selectedIndex == 0 ? 40 : 30,
+              color: _selectedIndex == 0 ? PRIMARY_COLOR : Colors.grey[600],
+            ),
+            label: "Tutorials",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.trending_up,
+              size: _selectedIndex == 1 ? 40 : 30,
+              color: _selectedIndex == 1 ? PRIMARY_COLOR : Colors.grey[600],
+            ),
+            label: "Graphs",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: _selectedIndex == 2 ? 40 : 30,
+              color: _selectedIndex == 2 ? PRIMARY_COLOR : Colors.grey[600],
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.local_library,
+              size: _selectedIndex == 3 ? 40 : 30,
+              color: _selectedIndex == 3 ? PRIMARY_COLOR : Colors.grey[600],
+            ),
+            label: "Learn",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+              size: _selectedIndex == 4 ? 40 : 30,
+              color: _selectedIndex == 4 ? PRIMARY_COLOR : Colors.grey[600],
+            ),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
