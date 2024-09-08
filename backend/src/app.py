3df1d3dc18d71ab71a6178f1b323b67f12e04c60
Syed Nafees Kaiser <oyonafees2001@gmail.com>
2024-09-08 13:25:17 +0600

    Add signed letter retrieve feature

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,9 +2,6 @@ from src import create_app
 
 app = create_app('development')
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
 from src.controller.sign_parameters_controller import *
 
 if __name__ == '__main__':
